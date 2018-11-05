477992228d476c73b076a46d5056f4b350eb824f
MartinPrejean <martin.prejean@hetic.net>
2018-11-05 18:00:49 +0100

    :sparkles: Add newTarget

--- script.js
+++ script.js
@@ -9,17 +9,39 @@ shooter.$start = shooter.$container.querySelector('.start')
 shooter.$score = shooter.$container.querySelector('.score .value')
 shooter.$timer = shooter.$container.querySelector('.timer')
 shooter.$targets = shooter.$container.querySelector('.targets')
+shooter.score = 0
 
 /**
  * Methods
  */
 shooter.addTarget = () => 
 {
+    //Create Element
     const $target = document.createElement('div')
     $target.classList.add('target')
     $target.style.top = `${Math.random() * 100}%`
     $target.style.left = `${Math.random() * 100}%`
     shooter.$targets.appendChild($target)
+
+    //listen to mouse enter
+    $target.addEventListener('mouseenter', () => 
+    {
+        shooter.shooterTarget($target)
+    })
 }
 
+shooter.shootTarget = (_$target) =>
+{
+    // Delete target
+    _$target.remove()
+
+    // Add new target
+    shooter.addTarget()
+
+    // Increment score
+    shooter.score++ 
+    shooter.$score.textContent = shooter.score
+
+    // Play sound
+}
 shooter.addTarget
